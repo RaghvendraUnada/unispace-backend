c715cbff39017c3d2768563030169b919555dcd1
RaghvendraUnada <raghvendra@unada.io>
2023-01-06 11:57:32 +0530

    uploaded codes

--- nameroute.js
+++ nameroute.js
@@ -2,9 +2,17 @@ const router = require("express").Router();
 const NameModal = require("./NameModal");
 
 router.post("/name", async (req, res) => {
-  console.log(req.body);
   try {
     const Data = await NameModal.create(req.body);
+    res.send(Data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/name", async (req, res) => {
+  try {
+    const Data = await NameModal.find({});
     res.send(Data);
   } catch (error) {
     console.log(error);
