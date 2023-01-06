163d577b9136bffddbac8c592176b31e9322061b
RaghvendraUnada <raghvendra@unada.io>
2023-01-06 13:26:26 +0530

    up

--- app.js
+++ app.js
@@ -12,14 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-app.use(cors())
-
-// app.use(
-//   cors({
-//     origin: "*",
-//     credentials: true,
-//   })
-// );
+app.use(cors());
 app.use(
   bodyParser.urlencoded({
     extended: false,
